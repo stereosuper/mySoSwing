7fe4597e677ba5609932e3470315544da70a70d8
adrienlementheour <adrien.le.mentheour@gmail.com>
2015-02-06 16:53:56 +0100

    Animation home

--- js/script.js
+++ js/script.js
@@ -10,3 +10,15 @@ $(document).ready(function(){
 $( window ).resize(function() {
 
 });
+
+$(window).load(function() {
+	var mySplitText = new SplitText("h1", {type:"lines"});
+	var tlHome = new TimelineMax;
+	tlHome.to($("#zone-gant"), 0.8, {x: "0px", y: "0px", rotation:"0deg", delay:0.2, ease:Quart.easeOut, onComplete:completeAnimGant});
+	tlHome.staggerFrom(mySplitText.lines, 0.9, {opacity: "0", y:"20px", ease:Quart.easeOut}, 0.2);
+	tlHome.to($("a#lien-app-store"), 0.9, {opacity: "1", y:"0px", ease:Quart.easeOut});
+});
+
+function completeAnimGant(){
+	TweenMax.set($("h1"), {opacity: "1"})
+}
