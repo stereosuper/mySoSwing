30f259bac8d07aed13aa40fbfeefa72f54721f90
ElisabethStereosuper <elisabeth@Chiot.local>
2015-02-10 18:41:19 +0100

    Mentions légales

--- README.md
+++
@@ -1,1 +0,0 @@
-# mySoSwing
--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
-///////////////
-// variables //
-///////////////
+function completeAnimGant(){
+	TweenMax.set($("h1"), {opacity: "1"})
+}
 
-
-$(document).ready(function(){
-
-});
-
-$( window ).resize(function() {
-
-});
-
-$(window).load(function() {
+function animHome(){
 	var mySplitText = new SplitText("h1", {type:"lines"});
 	var tlHome = new TimelineMax;
 	tlHome.to($("#zone-gant"), 0.8, {x: "0px", y: "0px", rotation:"0deg", delay:0.2, ease:Quart.easeOut, onComplete:completeAnimGant});
 	tlHome.staggerFrom(mySplitText.lines, 0.9, {opacity: "0", y:"20px", ease:Quart.easeOut}, 0.2);
 	tlHome.to($("a#lien-app-store"), 0.9, {opacity: "1", y:"0px", ease:Quart.easeOut});
-});
-
-function completeAnimGant(){
-	TweenMax.set($("h1"), {opacity: "1"})
 }
+
+$(function(){
+
+	$(window).load(function() {
+		if($('body').hasClass('home')){
+			animHome();
+		}
+	});
+
+	$(window).resize(function() {
+
+	});
+
+});
