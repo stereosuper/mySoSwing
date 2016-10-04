72413789443ed2402551e8f27f6949a3688518c8
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-10-04 10:44:45 +0200

    remove wp stuff

--- wordpress/wp-content/themes/mysoswing/searchform.php
+++
@@ -1,5 +0,0 @@
-<form role="search" method="get" class="searchform" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<fieldset>
-		<input type="search" name="s" class="searchInput" id="searchInput" value="<?php the_search_query(); ?>" placeholder="<?php _e('Rechercher', 'mysoswing'); ?>" /><input type="submit" class="btn searchSubmit" id="search" value="&#xe601;"/>
-	</fieldset>
-</form>
